e00f5f1e20e1b9d2d0a81885ba91c6d6c7d3dcf3
Vinay Omkumar Khilwani <vok8.khilwani@gmail.com>
2026-01-04 23:38:43 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
-import { Switch, Route, Router } from "wouter";
-import { useHashLocation } from "wouter/use-hash-location"; 
+import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -11,14 +10,12 @@ import Team from "@/pages/team";
 
 function Router() {
   return (
-    <Router hook={useHashLocation}>
     <Switch>
       <Route path="/" component={Home} />
       <Route path="/performance" component={Performance} />
       <Route path="/team" component={Team} />
       <Route component={NotFound} />
     </Switch>
-      </Router>
   );
 }
 
