a0ad81448708d943c9793b9d5f32ae5e856ddfd2
Vinay Omkumar Khilwani <vok8.khilwani@gmail.com>
2026-01-04 14:14:02 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,6 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
-export default {
-  base: '/zerix-capital-website/',
-}
-
 export default defineConfig({
   plugins: [
     react(),
