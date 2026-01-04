ec7aa684113254cb924bdce06545981a46145870
Vinay Omkumar Khilwani <vok8.khilwani@gmail.com>
2026-01-04 13:28:27 +0530

    Add files via upload

---
+++ client/src/App.tsx
@@ -0,0 +1,33 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import NotFound from "@/pages/not-found";
+import Home from "@/pages/home";
+import Performance from "@/pages/performance";
+import Team from "@/pages/team";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route path="/performance" component={Performance} />
+      <Route path="/team" component={Team} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Router />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
